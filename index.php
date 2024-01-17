511950561c8e627e3874731e1e8a6837785e5b88
Eduardo Malbert <emalbert@gmail.com>
2024-01-17 05:47:28 -0300

    Theme core files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Man template file.
+ * 
+ * @package Radikal
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Radikal Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
+
